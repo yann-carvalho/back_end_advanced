a335e40fefaeff3438eadd674071de6de3531247
Yann Carvalho <yanncarvalho95@gmail.com>
2024-08-21 19:12:05 -0300

    initial commit

---
+++ loja/src/main/java/com/minhaempresa/loja/modelo/Produto.java
@@ -0,0 +1,10 @@
+package com.minhaempresa.loja.modelo;
+
+import java.math.BigDecimal;
+
+public class Produto {
+    private Long id;
+    private String nome;
+    private String descricao;
+    private BigDecimal preco;
+}
